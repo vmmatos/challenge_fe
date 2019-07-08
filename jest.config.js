99915f572b1041ac3b9323584e334d8a73b67ce0
Vitor Matos <vmmatos@localhost.localdomain>
2019-07-08 17:43:40 +0100

    [FE] changed the package.json to run unit tests

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   moduleFileExtensions: ["js", "jsx", "json", "vue"],
   transform: {
     "^.+\\.vue$": "vue-jest",
-    ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$": "jest-transform-stub",
+    ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$":
+      "jest-transform-stub",
     "^.+\\.jsx?$": "babel-jest"
   },
   transformIgnorePatterns: ["/node_modules/"],
@@ -18,4 +19,4 @@ module.exports = {
     "jest-watch-typeahead/filename",
     "jest-watch-typeahead/testname"
   ]
-}
+};
